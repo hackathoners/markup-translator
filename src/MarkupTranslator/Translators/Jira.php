633d625f3ba4011958ffd2ba137f0e9fdf2abe1e
nAndy <nandy@wikia-inc.com>
2014-09-14 19:24:35 +0200

    added PHPDocs

--- src/MarkupTranslator/Translators/Jira.php
+++ src/MarkupTranslator/Translators/Jira.php
@@ -13,12 +13,15 @@ class Jira extends Base
     const STRONG_START_END = '*';
 
     /**
-     * Block elements are:
+     * Processing block elements. Block elements are:
      * - paragraph
      * - heading
      * - horizontal rule
      * - blockquote (multiline)
      * - code (multiline)
+     *
+     * @param string $text text to be proccessed
+     * @return string
      */
     protected function processBlock($text)
     {
@@ -42,6 +45,13 @@ class Jira extends Base
         return $this->processParagraph($text);
     }
 
+    /**
+     * Processes text wrapped in markup saying it's emphasized
+     *
+     * @param string $text text to be processed into a <em/> tag
+     * @param bool $inline flag telling if it's an inline blockquote or not
+     * @return string
+     */
     private function processBlockquote($text, $inline = false) {
         if( $inline ) {
             $length = 0;
@@ -60,6 +70,13 @@ class Jira extends Base
         return mb_substr($text, $end + $length);
     }
 
+    /**
+     * Looks for an end of blockqoute and returns the possition of the end
+     *
+     * @param string $text text being processed as blockqoute block
+     * @param bool $inline
+     * @return int
+     */
     private function findBlockquoteEnd($text, $inline = false)
     {
         $end = $this->lookAhead($text, ($inline ? "\n" : self::BLOCKQUOTE_BLOCK_START));
@@ -78,21 +95,45 @@ class Jira extends Base
         return $end;
     }
 
+    /**
+     * Processes text wrapped in markup saying it's emphasized
+     *
+     * @param string $text text to be processed into a <em/> tag
+     * @return string
+     */
     private function processEmphasized($text) {
         return $this->processWrapper($text, self::EMPHASIZED_START_END, self::NODE_EMPHASIZED);
     }
 
+    /**
+     * Processes text wrapped in markup saying it's important
+     *
+     * @param string $text text to be processed into a <strong/> tag
+     * @return string
+     */
     private function processStrong($text) {
         return $this->processWrapper($text, self::STRONG_START_END, self::NODE_STRONG);
     }
 
+    /**
+     * Common logic for wrapping markup such as <em/> and <strong/> tags
+     *
+     * @param string $text text to be processed
+     * @param string $formattingMarkup markup to process; one of class constants such as:
+     *      EMPHASIZED_START_END or STRONG_START_END
+     * @param string $nodeTag XML tag to be produced; one of class constants such as:
+     *      NODE_EMPHASIZED or NODE_STRONG
+     * @param int $markupLength
+     *
+     * @return string
+     */
     private function processWrapper($text, $formattingMarkup, $nodeTag, $markupLength = 1) {
         $formattedTextBegin = $this->lookAhead($text, $formattingMarkup);
         $beforeFormattedText = mb_substr($text, 0, $formattedTextBegin);
         $formattedTextEnd = $this->lookAhead(mb_substr($text, $formattedTextBegin + $markupLength), $formattingMarkup);
         $afterFormattedText = mb_substr(mb_substr($text, $formattedTextBegin + $markupLength), $formattedTextEnd + $markupLength);
         $formattedText = mb_substr($text, $formattedTextBegin + $markupLength, $formattedTextEnd);
-        
+
         if( $beforeFormattedText !== '' ) {
             $this->text($beforeFormattedText);
         }
@@ -114,6 +155,10 @@ class Jira extends Base
      * - links
      * - images
      * - emoticons
+     *
+     * @param string $text
+     *
+     * @return string
      */
     protected function processInline($text)
     {
